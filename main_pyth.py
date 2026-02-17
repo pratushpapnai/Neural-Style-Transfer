8f73a636aa47d5fb292f6218fc467bdf586eb227
Pratush <pratushpapnai@gmail.com>
2026-02-17 19:57:51 +0530

    modify main

--- main_pyth.py
+++ main_pyth.py
@@ -2,42 +2,18 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras.layers as tfl
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications import VGG19
+from tensorflow.keras.applications import VGG19,vgg19
 import os
 import cv2
 import matplotlib.pyplot as plt
+
+
 CONTENT_PATH="C:/Users/PratushPc/OneDrive/Documents/COURSES/DL SPECIALIZATION/4. Convolutional Neural Networks/4. Face Recognition - Neural Style Transfer/Custom Neural Style/images/content/"
 
 STYLE_PATH="C:/Users/PratushPc/OneDrive/Documents/COURSES/DL SPECIALIZATION/4. Convolutional Neural Networks/4. Face Recognition - Neural Style Transfer/Custom Neural Style/images/style/"
-content_image=cv2.imread(CONTENT_PATH+"louvre.jpg")
-print(content_image.shape)
-
-if content_image is None:
-    print("No Image Found")
-
-else:
-    content_image=cv2.cvtColor(content_image,cv2.COLOR_BGR2RGB)
-    content_image=content_image.astype("float")/255.
 
 
-generated_image=content_image.copy()
-noise=np.random.uniform(size=generated_image.shape,low=-0.25,high=0.25)
-generated_image+=noise
-generated_image=np.clip(generated_image,a_min=0.0,a_max=1.0)
-
-
-plt.figure(figsize=(10,10))
-
-plt.subplot(1,2,1)
-plt.title("Content Image")
-plt.axis("off")
-plt.imshow(content_image)
-
-plt.subplot(1,2,2)
-plt.title("Initialized Generated Image")
-plt.axis("off")
-plt.imshow(generated_image)
-base_model=VGG19(include_top=False,weights="imagenet",input_shape=(200,200,3))
+base_model=VGG19(include_top=False,weights="imagenet",input_shape=(400,400,3))
 base_model.trainable=False
 print(len(base_model.layers))
 
@@ -45,11 +21,8 @@ middle_layer=base_model.layers[12]
 print(middle_layer)
 base_model.layers
 intermediate_model=Model(inputs=base_model.input,outputs=middle_layer.output)
-intermediate_model.summary()
-def compute_content_cost(a_C,a_G):
 
-    print(a_C.shape)
-    
+def compute_content_cost(a_C,a_G):
     
     m,n_h,n_w,n_c=a_G.shape
 
@@ -59,12 +32,14 @@ def compute_content_cost(a_C,a_G):
     J_content=tf.reduce_sum((a_C_unrolled-a_G_unrolled)**2)/(4*n_c*n_h*n_w)
 
     return J_content
+
+
 STYLE_LAYERS={
-    "block1_conv2":0.4,
+    "block1_conv2":0.2,
     "block2_conv2":0.3,
-    "block3_conv2":0.1,
-    "block4_conv2":0.1,
-    "block5_conv2":0.1
+    "block3_conv2":0.4,
+    "block4_conv2":0.5,
+    "block5_conv2":0.6
 }
 
 
@@ -86,23 +61,46 @@ def compute_style_cost(a_S_array,a_G_array):
 
     return J_style
 
-@tf.function()
+
 def total_cost(J_content,J_style,alpha=10,beta=40):
     
     J_total=alpha*J_content + beta*J_style
     return J_total
-content_image=cv2.resize(content_image,(200,200))
-content_image=np.expand_dims(content_image,axis=0)
 
-generated_image=cv2.resize(generated_image,(200,200))
-generated_image=np.expand_dims(generated_image,axis=0)
 
-a_C=intermediate_model(content_image)
+content_image=cv2.imread(CONTENT_PATH+"dogesh.jpg")
+content_image=cv2.cvtColor(content_image,cv2.COLOR_BGR2RGB)
+content_image=cv2.resize(content_image,(400,400))
+content_image=content_image.astype("float32")/255.
+print(content_image.shape)
+
+plt.figure(figsize=(3,3))
+plt.imshow(content_image)
+plt.axis("off")
+
+
+style_image=cv2.imread(STYLE_PATH+"art.jpg")
+style_image=cv2.cvtColor(style_image,cv2.COLOR_BGR2RGB)
+style_image=cv2.resize(style_image,(400,400))
+style_image=style_image.astype("float32")/255.
+print(style_image.shape)
+
+plt.figure(figsize=(3,3))
+plt.imshow(style_image)
+plt.axis("off")
+
+generated_image=np.random.uniform(size=content_image.shape,low=0.0,high=1.0)
+
+print(generated_image.shape)
+
+plt.figure(figsize=(3,3))
+plt.axis("off")
+plt.imshow(generated_image)
+plt.show()
+
+a_C=intermediate_model(np.expand_dims(content_image,axis=0))
 print("a_C shape : ",a_C.shape)
 
-
-# J_content=compute_content_cost(a_C,a_G)
-# print(J_content)
 def get_layer_outputs(model):
 
     outputs=[model.get_layer(name).output for name in list(STYLE_LAYERS.keys())]
@@ -111,24 +109,11 @@ def get_layer_outputs(model):
     return temp_model
 
 vgg_layer_outputs=get_layer_outputs(base_model)
-
-style_image=cv2.imread(STYLE_PATH+"monet.jpg")
-if style_image is None:
-    print("No Image")
-else:
-    style_image=cv2.cvtColor(style_image,cv2.COLOR_BGR2RGB)
-    style_image=cv2.resize(style_image,(200,200))
-    style_image=style_image.astype("float")/255.
-    style_image=np.expand_dims(style_image,axis=0)
-    print(style_image.shape)
-
-a_S_array=vgg_layer_outputs(style_image)
+a_S_array=vgg_layer_outputs(np.expand_dims(style_image,axis=0))
 print(type(a_S_array),len(a_S_array))
 
-optimizer=tf.keras.optimizers.Adam(learning_rate=0.001)
-generated_image=tf.Variable(generated_image,dtype=tf.float32)
+optimizer=tf.keras.optimizers.Adam(learning_rate=0.01)
 
-@tf.function()
 def train_step(generated_image):
     with tf.GradientTape() as tape:
 
@@ -138,27 +123,47 @@ def train_step(generated_image):
         a_G_array=vgg_layer_outputs(generated_image)
         J_style=compute_style_cost(a_S_array,a_G_array)
 
-        J=total_cost(J_content,J_style)
+        J=total_cost(J_content,J_style,alpha=1,beta=50)
 
     grad=tape.gradient(J,generated_image)
     optimizer.apply_gradients([(grad,generated_image)])
     generated_image.assign(tf.clip_by_value(generated_image,0.0,1.0))
 
     return J
-from PIL import Image
+
 def tensor_to_image(tensor):
-    tensor=tensor*255
-    tensor=np.array(tensor,dtype=np.uint8)
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return tensor
 
-    if np.ndim(tensor)>3:
-        tensor=tensor[0]
 
-    return Image.fromarray(tensor)
+generated_image=generated_image.astype(np.float32)
+print(generated_image.dtype)
+
+generated_image=tf.Variable(np.expand_dims(generated_image,axis=0))
+# print(generated_image)
+
 epochs = 2501
 for i in range(epochs):
-    train_step(generated_image)
-    if i % 250 == 0:
-        print(f"Epoch {i} ")
-    if i % 250 == 0:
-        image = tensor_to_image(generated_image)
-        plt.imshow(image) 
+    loss=train_step(generated_image)
+    if i%250==0:
+        print(f"epoch {i}")
+        print(loss)
+        image=tensor_to_image(generated_image)
+        plt.figure(figsize=(4,4))
+        plt.imshow(image)
+        plt.axis("off")
+        plt.show()
+
+output_image=generated_image.numpy()
+print(output_image.shape)
+output_image=output_image[0]
+# output_image=cv2.resize(output_image,(800,600))
+output_image=(output_image*255).astype("int")
+
+# print(output_image*255)
+plt.imshow(output_image)
+print(output_image.shape)
